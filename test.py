e386e6233522ddaa7c4038a98b0c06a420159996
Kenneth Nyangweso <155610300+kennethnyangweso@users.noreply.github.com>
2026-05-13 12:50:21 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Test App", layout="centered")
-
-st.title("✅ Streamlit Is Working")
-st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Example.jpg/640px-Example.jpg")
-st.write("Hello from Streamlit!")
